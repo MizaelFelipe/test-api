360cf5bcb4d6ac8b6def57b3952f011d8d613523
Mizael Felipe <mizaelfelipe@outlook.com>
2022-09-13 00:29:21 -0300

    onde tudo comeco a dar certo!

--- api.js
+++ api.js
@@ -1,30 +1,6 @@
-function fazPost(url, body) {
-    console.log("Body=", body)
-    let request = new XMLHttpRequest()
-    request.open("POST", url, true)
-    request.setRequestHeader("Content-type", "application/json")
-    request.send(JSON.stringify(body))
+function fetchApiData() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then((Response) => Response.json())
+    .then((data) => console.log(data)); 
 
-    request.onload = function() {
-        console.log(this.responseText)
-    }
-
-    return request.responseText
-}
-
-
-function cadastraUsuario() {
-    event.preventDefault()
-    let url = "https://jsonplaceholder.typicode.com/posts"
-    let nome = document.getElementById("name").value
-    let email = document.getElementById("email").value
-    console.log(nome)
-    console.log(email)
-
-    body = {
-        "name": nome,
-        "email": email
-    }
-
-    fazPost(url, body)
 }
