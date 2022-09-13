e1c04e0e3a2cd6e1f0c0f0f229edb286a01c8717
Mizael Felipe <mizaelfelipe@outlook.com>
2022-09-13 20:52:03 -0300

    testando outras api`s

--- api.js
+++ api.js
@@ -14,3 +14,37 @@ function fetchApiData() {
     } ); 
 
 }
+
+function fetchApiDataa() {
+    fetch(' https://jsonplaceholder.typicode.com/users')
+    .then((Response) => Response.json())
+    .then(data =>{
+        const list = document.querySelector('#fill_list');
+
+        data.map((item) => {
+            const li = document.createElement('li');
+
+            li.setAttribute('id' , item.name);
+            li.innerHTML = item.name;
+            list.appendChild(li)
+        })
+    } ); 
+
+}
+
+function fetchApiDataaa() {
+    fetch(' https://jsonplaceholder.typicode.com/posts ')
+    .then((Response) => Response.json())
+    .then(data =>{
+        const list = document.querySelector('#fill_list');
+
+        data.map((item) => {
+            const li = document.createElement('li');
+
+            li.setAttribute('id' , item.body);
+            li.innerHTML = item.body;
+            list.appendChild(li)
+        })
+    } ); 
+
+}
